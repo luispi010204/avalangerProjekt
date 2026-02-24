3605604ce8a66675cdd8dc5f24d9a02d696998a6
luispi010204 <spinalui@students.zhaw.ch>
2026-02-24 09:06:00 +0100

    Added Country & State

--- src/main/java/ch/zhaw/avalanger/controller/AvalangeController.java
+++ src/main/java/ch/zhaw/avalanger/controller/AvalangeController.java
@@ -1,18 +1,28 @@
 package ch.zhaw.avalanger.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
-@RequestMapping("/api/avalange/")
+@RequestMapping("/api/avalange")
 public class AvalangeController {
 
-    @GetMapping("")
-    public String getAllAvalanges() {
-        return "No avalanges found.";
+    @GetMapping({"", "/{country}"})
+    public String getAllAvalanges(@PathVariable(required = false) String country, @RequestParam(required = false) String state) {
+        if (country == null || country.isEmpty()) {
+            return "No avalanges found";
+        }
+        return "No avalanges found for country: " + country + " and state: " + state;
     }
+
+
+
+
+
     
     
 }
