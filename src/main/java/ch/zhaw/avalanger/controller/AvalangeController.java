0f4d55142e4a3ce43ab453e90ab3416d030e1e36
luispi010204 <spinalui@students.zhaw.ch>
2026-02-24 09:22:43 +0100

    added Lombok

--- src/main/java/ch/zhaw/avalanger/controller/AvalangeController.java
+++ src/main/java/ch/zhaw/avalanger/controller/AvalangeController.java
@@ -3,8 +3,14 @@ package ch.zhaw.avalanger.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import ch.zhaw.avalanger.model.Avalange;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
@@ -18,6 +24,15 @@ public class AvalangeController {
         }
         return "No avalanges found for country: " + country + " and state: " + state;
     }
+
+
+    @PostMapping("")
+    public String postMethodName(@RequestBody Avalange  avalange) {
+        
+        
+        return "Avalange received: " + avalange.getCountry() + ", " + avalange.getState() + ", " + avalange.getDescription();
+    }
+    
 
 
 
